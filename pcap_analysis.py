a38d38a9bc39e13fa5e9dd0e0ed41e8d84b3814b
leez <shiqiba7@163.com>
2016-11-26 22:48:40 +0800

    修正一个包里面含有多个包时写入库中的问题，写成了一个语句，直接 insert into table values(),(),()

--- pcap_analysis.py
+++ pcap_analysis.py
@@ -5,15 +5,35 @@ import threading
 import time
 import datetime
 import pyshark
+import sys
 
+def w2log(str):
+
+	# print str
+
+	file_obj = open('pcap_analysis.log','a+')
+
+	try:
+
+		file_obj.write(str+'\n')
+
+	except:
+
+		print 'fail:',sys.exc_info()[0]
+
+	finally:
+
+		file_obj.close()
 
 class PcapHandler:
 
 	def __init__(self):
 
+		self.sql = ''
+
 		self.data = {}
 
-		self.sql = ''
+		self.sql_header = 'insert into pcap_packets (tcp_hdr_len,ip_addr,tcp_checksum,tcp_options_type,http_accept_language,http_request_version,tcp_flags_urg,eth_dst,pkt_transport_layer,eth_type,tcp_option_kind,tcp_options_type_class,tcp_options_timestamp_tsecr,http_request_method,tcp_checksum_good,ip_src_host,tcp_flags_res,http_connection,pkt_sniff_timestamp,tcp_flags_push,ip_proto,tcp_options_timestamp_tsval,tcp_options,http_request,http_expert_group,ip_host,tcp_window_size,http_accept,tcp_analysis_bytes_in_flight,ip_checksum,tcp_window_size_scalefactor,ip_ttl,ip_checksum_good,eth_ig,tcp_stream,tcp_flags_syn,tcp_len,tcp_options_type_copy,ip_id,http_request_uri,ip_dsfield_ecn,pkt_length,ip_dsfield,tcp_window_size_value,ip_checksum_bad,ip_version,http_content_length,tcp_flags_cwr,eth_src,http_cache_control,ip_len,ip_src,http_expert,ip_hdr_len,http_content_length_header,ip_flags_mf,http_content_type,ip_frag_offset,ip_flags_df,http_request_full_uri,tcp_flags_ns,http_expert_severity,http_expert_message,tcp_flags,http_user_agent,tcp_ack,ip_dst,ip_flags_rb,pkt_sniff_time,tcp_option_len,tcp_flags_fin,tcp_flags_ecn,tcp_checksum_bad,tcp_flags_reset,tcp_flags_ack,pkt_captured_length,tcp_options_type_number,ip_flags,pkt_number,ip_dsfield_dscp,http_host,ip_dst_host,tcp_nxtseq,tcp_dstport,tcp_srcport,tcp_seq,eth_addr,eth_lg,tcp_analysis,tcp_port) values '
 
 		self.pkt_struct = {
 			'pkt':['captured_length','length','sniff_time','sniff_timestamp','number','transport_layer'],
@@ -28,15 +48,25 @@ class PcapHandler:
 
 		cap = pyshark.FileCapture(pcap_file,display_filter='http')
 
+		w2log('this is cap:' + str(cap))
+
 		try:
+
+			w2log('now to load packets')
 
 			cap.load_packets()
 
+			w2log('loaded end')
+
 			len_cap = len(cap)
 
-			data = {}
+			w2log('all length:'+str(len_cap))
 
-			for j in range(len_cap):
+			for j in range (len_cap):
+
+				w2log('handing the No.'+str(j)+' File...')
+
+				cur_data = {}
 
 				#read each packet and write to db
 
@@ -48,67 +78,86 @@ class PcapHandler:
 
 						if not item == 'pkt':
 
-							data[cur_key] = cap[j][item].get_field_value(i)
+							cur_data[cur_key] = cap[j][item].get_field_value(i)
 
 						else:#each layer info 
 
-							data[cur_key] = eval('cap[j].'+i)
+							cur_data[cur_key] = eval('cap[j].'+i)
 
+				self.data[j] = cur_data
 
-			self.data = data
+			sql = self.createSQL(self.data)
+
+			w2log(sql)
+
+			self.wr2db(sql)
 
 		except:
 
-			print 'load failed'
-
-			self.data={}
+			w2log('load failed:'+str(sys.exc_info()[0]))
 
 		return self.data
 
-	def createSQL(self,data):
+	def createSQL(self,all_data):
 
-		if data:
+		w2log('alldata_type:'+str(type(all_data)))
 
-			sql = 'insert into pcap_packets ('
+		if all_data:
 
-			column_name = ''
+			values = ''
 
-			column_value = ''
+			for ite in all_data:
 
-			for item in data:
+				data = all_data[ite]
 
-				if not data[item] == '':
+				w2log('cur_data:'+str(data))
 
-					column_name += item+','
+				cur_value = '('
 
-					column_value += '"'+str(data[item])+'",'
+				for item in data:
 
-			column_name = column_name[:-1]#delete last ,
+					cur_value += '"' + str(data[item]) + '",'
 
-			column_value = column_value[:-1]#delete last ","
+					w2log('cur_value:'+str(cur_value))
 
-			sql = sql+column_name+') values('+column_value+')'
+				cur_value = cur_value[:-1] + '),'#delete last ","
 
-			self.sql = sql
+				values = values + cur_value
+
+			if values:
+
+				self.sql = self.sql_header + values[:-1]#delete last ","
+
+				w2log(self.sql)
+
+		else:
+
+			w2log('no data accept')
 
 		return self.sql
 
 	def wr2db(self,sql ,host="manager_db",db="managerpress",username="managerpress",passwd="managerpress"):
 
-		conn = mysql.connect(host,db,username,passwd)
-		
-		cursor = conn.cursor()
+		if sql:
 
-		# print sql
-
-		try:
-
-			cursor.execute(sql)
+			conn = mysql.connect(host,db,username,passwd)
 			
-			conn.commit()
+			cursor = conn.cursor()
 
-		except:
+			print sql
 
-			conn.rollback()
-		
-		conn.close()
+			try:
+
+				w2log('now try to write to db')
+
+				cursor.execute(sql)
+				
+				conn.commit()
+
+			except:
+
+				w2log('fail to insert :' + str(sys.exc_info()[0]))
+
+				conn.rollback()
+			
+			conn.close()
